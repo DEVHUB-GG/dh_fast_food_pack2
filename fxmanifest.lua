8bf72dc4bb6dc90ed3117683b7e3f4b641f97cb1
w. <71177337+Wiktor01@users.noreply.github.com>
2024-09-21 13:33:03 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+games {'gta5'}
+lua54 'yes'
+
+author 'DEVHUB (store.devhub.gg)'
+description 'FAST FOOD PROPS PACK 2'
+version '1.0.0'
+
+files {
+    "stream/dh_free_food_pack_2.ytyp",
+}
+
+data_file "DLC_ITYP_REQUEST" "stream/dh_free_food_pack_2.ytyp"
+
+escrow_ignore {
+    'stream/**/*',
+}
